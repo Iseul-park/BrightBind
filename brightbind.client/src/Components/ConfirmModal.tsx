5cac35254d2a1b3d28629e1f35875382a17914f3
Iseul Park <shannon.iseul.p@gmail.com>
2024-05-17 23:02:47 -0400

    clean confirm modal code

--- brightbind.client/src/Components/ConfirmModal.tsx
+++ brightbind.client/src/Components/ConfirmModal.tsx
@@ -1,9 +1,4 @@
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 
 interface ConfirmModalProps {
   title: string;
@@ -23,25 +18,23 @@ const ConfirmModal = ({ title, text, open, onConfirm, onClose }: ConfirmModalPro
   };
 
   return (
-    <>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">{text}</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleConfirm}>Delete</Button>
-          <Button onClick={handleClose} autoFocus>
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">{text}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleConfirm}>Delete</Button>
+        <Button onClick={handleClose} autoFocus>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
